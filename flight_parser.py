7724017688ae66f3c7bbeed65356ba7d0883876d
Aysel Abiyeva <ayselabiyeffam13@gmail.com>
2025-12-08 08:43:32 +0000

    fourth commit

--- flight_parser.py
+++ flight_parser.py
@@ -4,7 +4,7 @@ Python Final Assignment: Flight Schedule Parser and Query Tool
 
 Usage examples:
     python flight_parser.py -i data/db.csv
-    python flight_parser.py -d data/flights/
+    python flight_parser.py -d data/flights/ (example)
     python flight_parser.py -j db.json
     python flight_parser.py -j db.json -q data/query.json
 """
